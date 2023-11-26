d7712a5c7ea234cc8a8fd977a5735a660ef60d13
RayeZhang <152098671+RayeZhang@users.noreply.github.com>
2023-11-27 05:11:58 +0800

    Update aapc by country.R

--- aapc by country.R
+++ aapc by country.R
@@ -1,13 +1,14 @@
 library(ggplot2)
 data <- read.table("clipboard", sep = '\t', header = T)
 
-# 将国家设置为因子，并按照AAPC排序
+# Set 'countries' as a factor and sort by AAPC
 data$countries <- factor(data$countries, levels = data$countries[order(data$aapc)])
 
-# 定义颜色
+# Define colors
+# Use a specific yellow color for 'Asia', pinkish for positive AAPC, and bluish for negative AAPC
 asia_color <- ifelse(data$countries == "Asia", "#fce681", ifelse(data$aapc > 0, "#f5b0a5", "#75bfcf"))
 
-# 绘制水平柱状图
+# Create a horizontal bar chart
 p <- ggplot(data, aes(x = countries, y = aapc, fill = asia_color)) + 
   geom_bar(stat = 'identity', position = position_dodge(width = 0.6), width = 0.5) + 
   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, position = position_dodge(width = 0.6)) + 
@@ -22,5 +23,3 @@ p <- ggplot(data, aes(x = countries, y = aapc, fill = asia_color)) +
         axis.text.y = element_text(size = 9, face = "plain", family = "Arial", color = "black"))
 
 p
-
-
